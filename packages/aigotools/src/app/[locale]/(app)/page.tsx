98d89b95fce8f126166bdd79d4c7ee0718c4a71b
TerryLausy <61079533+terrylausy@users.noreply.github.com>
2026-05-17 05:42:21 +0800

    Add generateMetadata and update static params

--- packages/aigotools/src/app/[locale]/(app)/page.tsx
+++ packages/aigotools/src/app/[locale]/(app)/page.tsx
@@ -1,10 +1,25 @@
+import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
-
 import Container from "@/components/common/container";
 import Hero from "@/components/index/hero";
 import Search from "@/components/index/search";
 import SiteGroup from "@/components/common/sites-group";
 import { getFeaturedSites, getLatestSites } from "@/lib/actions";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const t = await getTranslations({
+    locale: params.locale,
+    namespace: "index",
+  });
+
+  return {
+    title: t("metadata.title"),
+  };
+}
 
 export default async function Page() {
   const t = await getTranslations("index");
@@ -24,6 +39,7 @@ export default async function Page() {
     </>
   );
 }
+
 export async function generateStaticParams() {
-  return [{ locale: 'en' }, { locale: 'cn' }];
+  return [{ locale: "en" }, { locale: "cn" }];
 }
