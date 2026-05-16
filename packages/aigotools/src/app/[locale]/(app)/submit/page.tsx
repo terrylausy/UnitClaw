7669201968e9b779c1144c42a5f5180ca9093033
TerryLausy <61079533+terrylausy@users.noreply.github.com>
2026-05-17 06:34:35 +0800

    Refactor Page component by removing metadata generation
    Removed metadata generation and simplified Page component.

--- packages/aigotools/src/app/[locale]/(app)/submit/page.tsx
+++ packages/aigotools/src/app/[locale]/(app)/submit/page.tsx
@@ -1,24 +1,7 @@
-import { Metadata } from "next";
-import { getTranslations } from "next-intl/server";
 import Container from "@/components/common/container";
 import NavBar from "@/components/common/nav-bar";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { locale: string };
-}): Promise<Metadata> {
-  const t = await getTranslations({
-    locale: params.locale,
-    namespace: "submit",
-  });
-
-  return {
-    title: t("metadata.title"),
-  };
-}
-
-export default async function Page() {
+export default function Page() {
   return (
     <Container>
       <NavBar name={["Submit"]} />
