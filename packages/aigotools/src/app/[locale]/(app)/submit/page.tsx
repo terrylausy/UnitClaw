be59dde1a76f87972f623d5b6f25da523ebf2ffb
TerryLausy <61079533+terrylausy@users.noreply.github.com>
2026-05-17 06:28:54 +0800

    Replace SubmitForm with a div on the submit page
    Removed SubmitForm component and added a div with 'Submit Page' text.

--- packages/aigotools/src/app/[locale]/(app)/submit/page.tsx
+++ packages/aigotools/src/app/[locale]/(app)/submit/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import Container from "@/components/common/container";
 import NavBar from "@/components/common/nav-bar";
-import SubmitForm from "@/components/submit/submit-form";
 
 export async function generateMetadata({
   params,
@@ -23,7 +22,7 @@ export default async function Page() {
   return (
     <Container>
       <NavBar name={["Submit"]} />
-      <SubmitForm />
+      <div className="p-6">Submit Page</div>
     </Container>
   );
 }
