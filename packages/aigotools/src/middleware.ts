c2a7f474e84a977a30d52eb3c8eef876860e5e3a
TerryLausy <61079533+terrylausy@users.noreply.github.com>
2026-05-17 03:44:30 +0800

    Refactor middleware to use createIntlMiddleware

--- packages/aigotools/src/middleware.ts
+++ packages/aigotools/src/middleware.ts
@@ -1,43 +1,21 @@
-import createMiddleware from "next-intl/middleware";
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import createIntlMiddleware from 'next-intl/middleware';
 
-import { localePrefix, locales } from "./navigation";
-import { AvailableLocales } from "./lib/locales";
-import { AppConfig } from "./lib/config";
-
-const intlMiddleware = createMiddleware({
-  locales: locales,
-  defaultLocale: AvailableLocales[0],
-  localePrefix: localePrefix,
+const intlMiddleware = createIntlMiddleware({
+  locales: ['en', 'zh'],
+  defaultLocale: 'en',
+  localePrefix: 'as-needed',
 });
 
-const isManageRoute = createRouteMatcher(["/(.*)/dashboard"]);
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
-const isUserRoute = createRouteMatcher(["/(.*)/submit"]);
-
-export default clerkMiddleware(
-  (auth, req) => {
-    if (isUserRoute(req)) auth().protect();
-
-    if (isManageRoute(req)) {
-      const { userId, redirectToSignIn } = auth();
-
-      if (!userId || !AppConfig.manageUsers.includes(userId)) {
-        return redirectToSignIn();
-      }
-    }
-
-    const nextPathname = req.nextUrl.pathname;
-
-    if (/^\/(api|trpc|sitemap)/.test(nextPathname)) {
-      return;
-    }
-
-    return intlMiddleware(req);
-  },
-  { debug: AppConfig.debugClerk }
-);
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+  return intlMiddleware(request);
+});
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
