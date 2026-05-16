eb06e59eba58cbd0e2078a68e9c063a3f16caaff
TerryLausy <61079533+terrylausy@users.noreply.github.com>
2026-05-17 01:58:22 +0800

    Simplify next.config.js by removing unused code
    Removed unused rewrites, redirects, and intl plugin.

--- packages/aigotools/next.config.js
+++ packages/aigotools/next.config.js
@@ -1,21 +1,10 @@
-const createNextIntlPlugin = require("next-intl/plugin");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [];
-  },
-  async redirects() {
-    return [];
-  },
-  sassOptions: {
-    // includePaths: [path.join(__dirname, 'styles')],
-  },
+  reactStrictMode: true,
   images: {
-    remotePatterns: [],
+    unoptimized: true,
   },
-};
+  generateSitemaps: false
+}
 
-const withNextIntl = createNextIntlPlugin();
-
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig
