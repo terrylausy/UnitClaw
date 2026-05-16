66d35b1c97bba078b01bcdd479b860380ca4ecf4
TerryLausy <61079533+terrylausy@users.noreply.github.com>
2026-05-17 06:17:49 +0800

    Make searchParams properties optional in Page function

--- packages/aigotools/src/app/[locale]/(app)/search/page.tsx
+++ packages/aigotools/src/app/[locale]/(app)/search/page.tsx
@@ -23,12 +23,12 @@ export default async function Page({
   searchParams,
 }: {
   searchParams: {
-    s: string;
-    c: string;
+    s?: string;
+    c?: string;
   };
 }) {
-  const rawCategory = searchParams["c"] || "";
-  const rawSearch = searchParams["s"] || "";
+  const rawCategory = searchParams.c || "";
+  const rawSearch = searchParams.s || "";
   const category = decodeURIComponent(rawCategory.toString());
   const search = decodeURIComponent(rawSearch.toString());
 
